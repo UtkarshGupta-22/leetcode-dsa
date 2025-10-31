72666fa9cf793a6e82b949adabe44169d794f5b9
Utkarsh Gupta <utkarshg646@gmail.com>
2025-10-31 13:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (9.16%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> temp(n,0);
+        int index=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]!=0){
+                temp[index] = nums[i];
+                index++;
+            }
+        }
+        for(int i=0; i<n; i++){
+            nums[i] = temp[i];
+        }
+        return;
+    }
+};
