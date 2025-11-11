096b19edea3e981b30da0e3d59e49948beb2ab33
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-11 15:43:46 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (61.88%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -25,16 +25,11 @@ public:
     for(int i=0; i<v.size(); i++){
         if(el1 == v[i]) cnt1++;
         if(el2 == v[i]) cnt2++;
-
     }
     int mini = (int)(v.size()/3) + 1;
     if(cnt1 >= mini) ls.push_back(el1);
     if(cnt2 >= mini) ls.push_back(el2);
     sort(ls.begin(),ls.end());
     return ls;
-
-
-
-
     }
 };
