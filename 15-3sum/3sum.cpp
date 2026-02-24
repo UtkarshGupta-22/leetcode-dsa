b043cfd55e2a2eb56a0e241bb936d0d3a904af4b
Utkarsh Gupta <utkarshg646@gmail.com>
2026-02-24 23:58:13 +0530

    Time: 59 ms (22.43%) | Memory: 29.2 MB (30.72%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,62 +1,35 @@
 class Solution {
 public:
-
-
-
-
-
-
-
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
         int n = nums.size();
-        for(int i=0; i<n; i++){
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<n-2; i++){
+            
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int j = i+1;
-            int k = n-1;
-            while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum<0){
-                    j++;
-                }
-                else if (sum>0){
-                    k--;
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int sum = -1*nums[i];
+                if(nums[left]+nums[right] == sum){
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                    
+                    while(left<right && nums[left]==nums[left+1]) left++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
+
+                    left++;
+                    right--;
+
+                } 
+                else if(nums[left]+nums[right] > sum){
+                    right--;
                 }
                 else{
-                    vector<int> temp = {nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k] == nums[k+1]) k--;
+                    left++;
                 }
-
+                
             }
         }
         return ans;
-    
-    
-    
-    
-    
-    
-    
-    //     int n = nums.size();
-    //     set<vector<int> > st;
-    //     for(int i=0; i<n; i++){
-    //         set<int> hashset;
-    //         for(int j=i+1; j<n; j++){
-    //             int third = -(nums[i] + nums[j]);
-    //             if(hashset.find(third)!=hashset.end()){
-    //                 vector<int>ans = {nums[i],nums[j],third};
-    //                 sort(ans.begin(),ans.end());
-    //                 st.insert(ans);
-    //             }
-    //             hashset.insert(nums[j]);
-    //         }
-    //     }
-    //     vector<vector<int>> ans2(st.begin(),st.end());
-    //     return ans2;
     }
 };
