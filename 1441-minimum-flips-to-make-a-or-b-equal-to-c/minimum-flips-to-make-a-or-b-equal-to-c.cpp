6e941dc72b339f9fb24cd51fad0e8697b3bbac19
Utkarsh Gupta <utkarshg646@gmail.com>
2026-03-23 10:55:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (38.24%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
+        //Approach 2
         
-        int flips=0;
+        int result = (a|b)^c;
 
-        while(a!=0 || b!=0 || c!=0){
-            
-            if((c & 1)==1){
+        int result1 = (a&b);
 
-                if((a & 1) == 0 && (b & 1) == 0) flips++;
-            }
-            else{
-                if((a & 1)==1) flips++;
-                if((b & 1)==1) flips++;
-            }
+        int result2 = (result1 & result);
 
-            a>>=1;
-            b>>=1;
-            c>>=1;
-        }
-        return flips;
+        return __builtin_popcount(result) + __builtin_popcount(result2);
+        
     }
 };
