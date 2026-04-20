9e7c15880604a171d246e9d74e8ef7615c1c17c5
Utkarsh Gupta <utkarshg646@gmail.com>
2026-04-20 09:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (44.64%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.cpp
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -11,31 +11,15 @@
  */
 class Solution {
 public:
-    bool isOnePresent(TreeNode* node){
-        if(node==NULL) return false;
-
-        if(node->val==1) return true;
-
-        return isOnePresent(node->left) || isOnePresent(node->right);
-    }
     TreeNode* pruneTree(TreeNode* root) {
         if(root==NULL) return NULL;
 
-        if(!isOnePresent(root->left)){
-            root->left =NULL;
-        }
-        if(!isOnePresent(root->right)){
-            root->right = NULL;
-        }
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
 
-        pruneTree(root->left);
-
-        pruneTree(root->right);
-
-        if(root->left==NULL && root->right==NULL && root->val==0){
+        if(root->left==NULL && root->right==NULL && root->val==NULL){
             return NULL;
         }
-
         return root;
     }
 };
