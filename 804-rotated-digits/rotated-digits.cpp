955be0a06f7ca63508d31d596d125c3ab1746804
Utkarsh Gupta <utkarshg646@gmail.com>
2026-05-02 23:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (83.68%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isGood(int n){
+        bool change=false;
+        while(n>0){
+            int d = n%10;
+            if(d == 3 || d == 4 || d == 7) return false;
+            if(d== 2 || d == 5 || d == 6 || d == 9) change = true;
+
+            n = n/10;
+        }
+        return change;
+    }
+    int rotatedDigits(int n) {
+        int count=0;
+        for(int i=1; i<=n; i++){
+            if(isGood(i)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
