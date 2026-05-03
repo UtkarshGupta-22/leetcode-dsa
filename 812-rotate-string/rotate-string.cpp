d2546060df9e59710c8a03aa4b89df9acdd683b4
Utkarsh Gupta <utkarshg646@gmail.com>
2026-05-03 22:21:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (84.87%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,6 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         if(s.length() != goal.length()) return false;
         string temp = s + s;
-        return temp.find(goal) != string::npos;
+        if (temp.find(goal) != -1) return true;
+        return false;
     }
 };
