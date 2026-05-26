70d1514d8c808e820bfbe726205429edc3ee2950
Utkarsh Gupta <utkarshg646@gmail.com>
2026-05-26 09:20:24 +0530

    Time: 1 ms (22.32%) | Memory: 27.2 MB (25.32%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,34 +2,26 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        //previous greatest element
-        int prev[n];
-        prev[0] = -1;
-        int max = height[0];
-        for(int i=1; i<n; i++){
-            prev[i] = max;
-            if(max<height[i]) max = height[i];
-        }
-        //Next greatest element
-        int next[n];
-        next[n-1] = -1;
-        max = height[n-1];
-        for(int i=n-2; i>=0; i--){
-            next[i] = max;
-            if(max<height[i]) max = height[i];
-        }
-        //minimum
-        int mini[n];
-        for(int i=0; i<n; i++){
-            mini[i] = min(prev[i],next[i]);
-        }
-        //calculating water
-        int water=0;
-        for(int i=1; i<n-1; i++){
-            if(height[i]<mini[i])
-            water += (mini[i]-height[i]);
-        }
-        return water;
 
+        vector<int>leftMax(n);
+        vector<int> rightMax(n);
+
+        leftMax[0] = height[0];
+
+        rightMax[n-1] = height[n-1];
+
+        for(int i=1; i<n; i++){
+            leftMax[i] = max(leftMax[i-1],height[i]);
+        }
+        for(int j =n-2; j>=0; j--){
+            rightMax[j] = max(rightMax[j+1],height[j]);
+        }
+        int sum = 0;
+        int water;
+        for(int idx=0; idx<n; idx++){
+            water = min(leftMax[idx],rightMax[idx]) - height[idx];
+            sum += water;
+        }
+        return sum;
     }
 };
