61da96fb8ce576417fe117d57650ef44cf529736
Utkarsh Gupta <utkarshg646@gmail.com>
2026-06-08 13:01:37 +0530

    Time: 26 ms (14.04%) | Memory: 21.2 MB (15.79%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    void markColn(vector<vector<int>>&matrix,int j,int n, int m){
-        for(int i=0; i<n; i++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-10;
-            }
-        }
-    }
-    void markRow(vector<vector<int>>&matrix, int i, int n, int m){
-        for(int j=0; j<m; j++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-10;
-            }
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]==0){
-                    markRow(matrix,i,n,m);
-                    markColn(matrix,j,n,m);
+        int m = matrix.size();
+
+        int n = matrix[0].size();
+
+        vector<vector<int>> temp;
+        temp = matrix;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<n; k++){
+                        temp[i][k] = 0; //marking each row as 9
+                    }
+                    for(int k=0; k<m; k++){
+                        temp[k][j] =0; //marking each column as 0;
+                    }
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]==-10){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-
-
+        matrix = temp;
     }
 };
