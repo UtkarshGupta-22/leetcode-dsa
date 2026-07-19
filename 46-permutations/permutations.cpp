d0e3eb471deb0ee4ef2129c2334ba320052ffa15
Utkarsh Gupta <utkarshg646@gmail.com>
2026-07-20 05:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (71.95%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public: 
+    void findPermutation(vector<int>&nums, vector<int>& ds, vector<vector<int>>&ans, int freq[]){
+        if(ds.size() == nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(!freq[i]){
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                findPermutation(nums,ds,ans,freq);
+                freq[i]=0;
+                ds.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        int n = nums.size();
+        int freq[n];
+        for(int i=0; i<n; i++){
+            freq[i] = {0};
+        }
+        findPermutation(nums,ds,ans,freq);
+        return ans;
+
+    }
+};
