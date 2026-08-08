4ea136a2d43e2fa9742db9f72043e30d6555ebca
Utkarsh Gupta <172594313+UtkarshGupta-22@users.noreply.github.com>
2026-08-09 01:52:06 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (34.45%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,26 +4,18 @@ public:
         int n = height.size();
         vector<int>leftMax(n);
         vector<int>rightMax(n);
-        leftMax[0] = height[0];
-        rightMax[n-1] = height[n-1];
-
-        for(int i=1; i<n; i++){
+        leftMax[0]=height[0];
+        for(int i=1; i<n;i++)
             leftMax[i] = max(leftMax[i-1],height[i]);
-        }
-        for(int j=n-2; j>=0; j--){
-            rightMax[j] = max(rightMax[j+1],height[j]);
-        }
-        
-        int sum = 0;
+        rightMax[n-1] = height[n-1];
+        for(int j=n-2; j>=0; j--) rightMax[j] = max(rightMax[j+1],height[j]);
+
         int water;
-        for(int idx=0; idx<n; idx++){
+        int sum=0;
+        for(int idx=0;idx<n; idx++){
             water = min(leftMax[idx],rightMax[idx]) - height[idx];
             sum += water;
         }
         return sum;
-        
-
-        
-
     }
 };
