e448b8cb71e17601d9efa0628c429a3eabb0a2b4
Utkarsh Gupta <172594313+UtkarshGupta-22@users.noreply.github.com>
2026-08-12 20:49:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (24.22%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
-    vector<int>dp;
-    int f(vector<int> arr, int i){  //recursive
-        if(i== arr.size()-1) return arr[i]; //1 house
-        if(i==arr.size()-2) return max(arr[i],arr[i+1]); //2 houses
-        return max(arr[i]+f(arr,i+2),0+f(arr,i+1));
+    int f(int idx, vector<int> &arr,vector<int> &dp){
+        if(idx==0) return arr[idx];
+        if(idx<0) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        
+        int pick = arr[idx] + f(idx-2,arr,dp);
+        int notPick = 0 + f(idx-1,arr,dp);
+        
+        return dp[idx] = max(pick,notPick);
     }
-
-    int ftd(vector<int> arr, int i){  //recursive
-        if(i== arr.size()-1) return arr[i]; //1 house
-        if(i==arr.size()-2) return max(arr[i],arr[i+1]); //2 houses
-
-        if(dp[i]!=-1) return dp[i]; //ith already computer
-        //if dp[i] was -1 so let's compute ith state for first time
-        return dp[i] = max(arr[i]+ftd(arr,i+2),0+ftd(arr,i+1));
-    }
-
-    int fbu(vector<int>&arr){
+    int rob(vector<int> &arr) {
         int n = arr.size();
-        if(n==1) return arr[0];
-        dp.clear();
-        dp.resize(n);
-        dp[n-1] = arr[n-1];
-        dp[n-2] = max(arr[n-1],arr[n-2]);
-        for(int i=n-3; i>=0; i--){
-            dp[i] = max(arr[i]+dp[i+2],0 + dp[i+1]);
-        }
-
-        return dp[0];
-    }
-
-
-
-
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        dp.clear();
-        dp.resize(105,-1);
-        return fbu(nums);    
+        vector<int>dp(n+1,-1);
+        return f(n-1,arr,dp);
     }
 };
