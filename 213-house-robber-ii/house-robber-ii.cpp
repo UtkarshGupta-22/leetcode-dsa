706164de9852c46b4a24a46f291a9333e5dd4a64
Utkarsh Gupta <172594313+UtkarshGupta-22@users.noreply.github.com>
2026-09-02 09:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (64.59%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -10,18 +10,36 @@ public:
         
         return dp[idx] = max(pick,notPick);
     }
+     int f2(vector<int> &arr) {
+         int n = arr.size();
+        // vector<int>dp(n+1,0);
+        // return f2(n-1,arr,dp);
+
+        int prev = arr[0];
+        int prev2 = 0;
+
+        for (int i=1; i<n; i++){
+            int take = arr[i];
+            if(i>1) take +=prev2;
+            int nottake = 0 + prev;
+
+            int curi = max(take,nottake);
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
+    }
     int rob(vector<int>& nums) {
 
         int n = nums.size();
         if(n==0) return 0;
         if(n==1) return nums[0];
-        vector<int>dp(n+1,-1);
-        vector<int>dp2(n+1,-1);
+        
         vector<int>temp,temp2;
         for(int i=0; i<n; i++){
             if(i!=0) temp.push_back(nums[i]);
             if(i!=n-1) temp2.push_back(nums[i]);
         }
-        return max(f(temp.size()-1,temp,dp),f(temp2.size()-1,temp2,dp2));
+        return max(f2(temp),f2(temp2));
     }
 };
