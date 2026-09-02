a4dff4ed15f2de2e6519ef9a95d210987165eb74
Utkarsh Gupta <172594313+UtkarshGupta-22@users.noreply.github.com>
2026-09-02 22:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.24%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Top Down ---> Memoisation
     int f(int i, int j, vector<vector<int>> &dp){
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
@@ -9,8 +10,22 @@ public:
         return dp[i][j] = up + left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp);
+        int dp[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up,left;
+                    up=0;
+                    left=0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+
     
     }
 };
