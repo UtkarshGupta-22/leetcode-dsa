c31cb386423f05daefe47d3b000eaa1544f5c4f9
Utkarsh Gupta <172594313+UtkarshGupta-22@users.noreply.github.com>
2026-09-02 08:46:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (90.30%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -11,20 +11,36 @@ public:
         return dp[idx] = max(pick,notPick);
     }
 
-    int f2(int idx, vector<int> &arr,vector<int> &dp){
-        dp[0] = arr[0];
-        int n = arr.size();
-        for (int i=1; i<n; i++){
-            int pick = arr[i];
-            if(i>1) pick += dp[i-2];
-            int notpick = 0 + dp[i-1];
-            dp[i] = max(pick,notpick);
-        }
-        return dp[n-1];
-    }
+   // int f2(int idx, vector<int> &arr,vector<int> &dp){
+        // dp[0] = arr[0];
+        // int n = arr.size();
+        // for (int i=1; i<n; i++){
+        //     int pick = arr[i];
+        //     if(i>1) pick += dp[i-2];
+        //     int notpick = 0 + dp[i-1];
+        //     dp[i] = max(pick,notpick);
+        // }
+       
+
+       // return dp[n-1];
+    //}
     int rob(vector<int> &arr) {
-        int n = arr.size();
-        vector<int>dp(n+1,0);
-        return f2(n-1,arr,dp);
+         int n = arr.size();
+        // vector<int>dp(n+1,0);
+        // return f2(n-1,arr,dp);
+
+        int prev = arr[0];
+        int prev2 = 0;
+
+        for (int i=1; i<n; i++){
+            int take = arr[i];
+            if(i>1) take +=prev2;
+            int nottake = 0 + prev;
+
+            int curi = max(take,nottake);
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
     }
 };
