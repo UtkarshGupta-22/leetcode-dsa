2ee095071d8eab4539e184ef1e96527366ff5ac1
Utkarsh Gupta <172594313+UtkarshGupta-22@users.noreply.github.com>
2026-09-14 23:43:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -4,13 +4,17 @@ public:
         if(ind==0) return 1;
         if(ind==1) return 1;
         if(dp[ind]!=-1) return dp[ind];
-        int left = f(ind-1,dp);
-        int right = f(ind-2,dp);
+        
 
-        return dp[ind] = left+right;
+        return dp[ind] = f(ind-1,dp)+f(ind-2,dp);
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return f(n,dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
