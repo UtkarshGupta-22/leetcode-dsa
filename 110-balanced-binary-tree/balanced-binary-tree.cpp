e7f0da975f914ce1bf017c8044834e4bab43d6a4
Utkarsh Gupta <172594313+UtkarshGupta-22@users.noreply.github.com>
2026-09-16 22:01:08 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (83.36%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -17,12 +17,10 @@ public:
 
     int height(TreeNode* root){
         if(root==NULL) return 0;
-
         int lh = height(root->left);
         if(lh==-1) return -1;
         int rh = height(root->right);
         if(rh==-1) return -1;
-
         if(abs(lh-rh)>1) return -1;
         return 1 + max(lh,rh);
     }
